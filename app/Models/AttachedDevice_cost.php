95a769f6e9e92dc3d96efb15137213ec326d7b3b
Shi-Chin <fred3571588@gmail>
2022-05-13 08:24:30 +0800

    fix: model名稱修正

--- app/Models/AttachedDevice_cost.php
+++ app/Models/AttachedDevice_cost.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AttachedDevice_cost extends Model
+class AttachedDevice_Cost extends Model
 {
     use HasFactory;
 
