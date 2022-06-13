ccd45a64575b853881661906d2d4eb19c278744a
Shi-Chin <fred3571588@gmail>
2022-06-13 09:51:20 +0800

    debug

--- app/Http/Controllers/ntpc_connect_controller.php
+++ app/Http/Controllers/ntpc_connect_controller.php
@@ -43,7 +43,7 @@ class ntpc_connect_controller extends Controller
 
         $userinfo =  json_encode($get_ntpc_userinfo->json(), JSON_UNESCAPED_UNICODE);
         $user_pass = false;
-
+        dd($userinfo);
         if ($userinfo['category'] != 'e') {
             $user_pass = false;
         } elseif ($userinfo['certificate']['category'] != 'MOEACA') {
