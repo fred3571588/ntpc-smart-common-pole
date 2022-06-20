7390ae1aa969c8af06ae714184fd9fb51b111f07
Shi-Chin <fred3571588@gmail>
2022-06-20 16:07:13 +0800

    debug

--- app/Http/Controllers/B00_Announcement/B01_Announcement_Manage_controller.php
+++ app/Http/Controllers/B00_Announcement/B01_Announcement_Manage_controller.php
@@ -87,7 +87,7 @@ class B01_Announcement_Manage_controller extends Controller
     {
         //
         $announcement = Announcement::findOrFail($id);
-        dd($announcement);
+        // dd($announcement);
         $announcement->update([
             'announcement_at' => $request->announcement_at,
             'place' => $request->place,
